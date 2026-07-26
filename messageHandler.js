c72f3d67a99fecc5a61da5ee40a8cb8242ab277a
TOPFEROS-TECH <topferostech@gmail.com>
2026-07-26 12:31:40 -0400

    Create messageHandler.js

---
+++ messageHandler.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+const path = require("path");
+
+async function messageHandler(sock, message) {
+
+    if (!message.messages) return;
+
+    const msg = message.messages[0];
+
+    if (!msg.message) return;
+
+    if (msg.key.fromMe) return;
+
+    const pluginsPath = path.join(__dirname, "../plugins");
+
+    if (!fs.existsSync(pluginsPath)) return;
+
+    const plugins = fs.readdirSync(pluginsPath)
+        .filter(file => file.endsWith(".js"));
+
+    for (const file of plugins) {
+
+        try {
+
+            const plugin = require(path.join(pluginsPath, file));
+
+            if (typeof plugin.run === "function") {
+                await plugin.run(sock, msg);
+            }
+
+        } catch (err) {
+
+            console.error(`Plugin Error (${file})`, err);
+
+        }
+
+    }
+
+}
+
+module.exports = messageHandler;
