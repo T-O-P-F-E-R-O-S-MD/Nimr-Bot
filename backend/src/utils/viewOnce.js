a50974c664829487917d9bb11106c5868cc68729
TOPFEROS-TECH <topferostech@gmail.com>
2026-07-26 13:59:28 -0400

    Create backend/src/utils/viewOnce.js

---
+++ backend/src/utils/viewOnce.js
@@ -0,0 +1,14 @@
+/**
+ * ============================================
+ * TOPFEROS MD - VIEW ONCE
+ * Developer: TOPFEROS TECH
+ * ============================================
+ */
+
+async function handleViewOnce(sock, msg) {
+    // View Once logic will be implemented here.
+}
+
+module.exports = {
+    handleViewOnce
+};
